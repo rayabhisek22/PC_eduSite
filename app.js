a295bad7f0a894d65c3930b80e177bfe5029672d
Gaurav Das <gauravdas014@gmail.com>
2020-06-29 14:57:16 +0530

    change var to const

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-var express = require('express'),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	LocalStrategy = require('passport-local');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
-var app = express();
+const app = express();
 
 //Database config===================================
-var url = 'mongodb://localhost:27017/newdb';
-mongoose.connect(url, {useNewUrlParser: true, useFindAndModify:false, useUnifiedTopology: true}, err=>{
-	if(err) console.log(err);
-	else
-		console.log("connected");
-});
+const url = "mongodb://localhost:27017/PC";
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("DB connected");
+  }
+);
 
 //App config=======================================
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routes===========================================
-app.use(express.static('public'));
-var indexRoutes = require('./routes/index');
+app.use(express.static("public"));
+var indexRoutes = require("./routes/index");
 app.use(indexRoutes);
 
-
 //Application listen at port 8080
-var port = 5000| process.env.PORT;
-app.listen(port,(err)=>{
-	if(err)
-		console.log(err);
-	else
-		console.log("Server started at port 5000");
-}) 
+var port = 5000 | process.env.PORT;
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  else console.log("Server started at port 5000");
+});
