6fbe9cf8f7ce690040a4884a7ec1022922d314de
Gaurav Das <gauravdas014@gmail.com>
2020-06-29 16:50:58 +0530

    add navbar

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const facultyRoutes = require("./Faculty/faculty");
 const studentRoutes = require("./Student/student");
 const adminRoutes = require("./Admin/admin");
 
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
