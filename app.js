14d80ad2e7aadf8170d42257e7e1dcaa12b6f0e3
rayabhisek22 <rayabhisek22@gmail.com>
2020-07-07 12:18:27 +0530

    Added package

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const url = "mongodb+srv://pragati:pragati123@cluster0.fqhit.mongodb.net/PC?retryWrites=true&w=majority/";
 const urlLocal = "mongodb://localhost:27017/PC"
 mongoose.connect(
-  urlLocal,
+  url,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
