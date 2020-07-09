b88c64bf51dab6e3b31f273bdb7066bac4f47324
Gaurav Das <gauravdas014@gmail.com>
2020-07-09 15:56:04 +0530

    add main center page

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const app = express();
 //Database config===================================
 const url =
   "mongodb+srv://pragati:pragati123@cluster0.fqhit.mongodb.net/PC?retryWrites=true&w=majority/";
-// const urlLocal = "mongodb://localhost:27017/PC";
+const urlLocal = "mongodb://localhost:27017/PC";
 mongoose.connect(
-  url,
+  urlLocal,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
